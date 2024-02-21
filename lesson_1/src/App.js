7f7748d5e5830d18995bb03a0e6215a3bb960874
UsmanalievI <uin.2201@gmail.com>
2024-02-21 13:27:22 +0600

    Lesson2

---
+++ lesson_1/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Button from './components/button/Button';
+import User from './components/user/User';
+import Example from './components/example/example';
+import Header from './components/header/Header';
+
+function App() {
+  const navbar=['home', 'about us', 'contacts']; 
+
+  return (
+    <>
+      <Button/>
+    </>
+  );
+}
+
+export default App;
